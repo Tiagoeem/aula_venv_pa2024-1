042cbf9c6c60356a3956177833d9c2d4cd4feb7e
Tiago Sanches - Low <tiago.eem@gmail.com>
2024-02-09 11:19:32 -0300

    Meu primeiro commit

---
+++ meu_python.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+a = 3
+b = 2
+
+c = a + b
+
+print("olá mundo: ", c)
+
+
+print("olá mundo 2: ", c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
